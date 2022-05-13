7879c11cc30d8bf79fbe1a39f5a5371d96793d83
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-05-13 15:32:27 -0500

    certs

--- bin/cert.ps1
+++ bin/cert.ps1
@@ -1,15 +1,8 @@
-mkcert -install ttvDevRig
-mkcert ttvDevRig
-$cert = Get-Content ttvDevRig.pem
-Remove-Item ./ttvDevRig.pem
-$key = Get-Content ttvDevRig-key.pem 
-Remove-Item  ./ttvDevRig-key.pem
-$key + $cert | Out-File -FilePath ./node_modules/.cache/webpack-dev-server/server.pem
+mkcert -install -cert-file ./cert.pem -key-file ./key.pem localhost
+$cert = Get-Content ./cert.pem
+$key = Get-Content ./key.pem
 
-mkcert -install localhost
-mkcert localhost
-$cert = Get-Content localhost.pem
-Remove-Item ./localhost.pem
-$key = Get-Content localhost-key.pem 
-Remove-Item ./localhost-key.pem
-$key + $cert | Out-File -FilePath ./certificates/localhost.pem
+$cert + $key | Out-File -FilePath ./node_modules/.cache/webpack-dev-server/server.pem
+
+Remove-Item ./cert.pem
+Remove-Item ./key.pem
